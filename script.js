16cd8acd2a7d713aadd57cce1026f182498423aa
WGE <wge2024@gmail.com>
2024-12-22 12:51:55 -0500

    Added the missing src=script to bottom of pages

--- script.js
+++ script.js
@@ -1,30 +1,36 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const lazyVideos = [].slice.call(document.querySelectorAll('.lazy-youtube'));
+// document.addEventListener('DOMContentLoaded', function () {
+//   const lazyVideos = [].slice.call(document.querySelectorAll('.lazy-youtube'));
 
-  lazyVideos.forEach(video => {
-    video.addEventListener('click', function () {
-      const videoId = this.getAttribute('data-videoid');
-      const iframe = document.createElement('iframe');
-      iframe.setAttribute('src', `https://www.youtube.com/embed/${videoId}?autoplay=1&modestbranding=1&rel=0`);
-      iframe.setAttribute('frameborder', '0');
-      iframe.setAttribute('allow', 'autoplay; encrypted-media');
-      iframe.setAttribute('allowfullscreen', '');
+//   lazyVideos.forEach(video => {
+//     video.addEventListener('click', function () {
+//       const videoId = this.getAttribute('data-videoid');
+//       const iframe = document.createElement('iframe');
+//       iframe.setAttribute('src', `https://www.youtube.com/embed/${videoId}?autoplay=1&modestbranding=1&rel=0`);
+//       iframe.setAttribute('frameborder', '0');
+//       iframe.setAttribute('allow', 'autoplay; encrypted-media');
+//       iframe.setAttribute('allowfullscreen', '');
 
-      // Remove the button and insert the iframe
-      this.innerHTML = '';
-      this.appendChild(iframe);
-    });
-  });
+//       // Remove the button and insert the iframe
+//       this.innerHTML = '';
+//       this.appendChild(iframe);
+//     });
+//   });
+// });
+
+document.addEventListener('DOMContentLoaded', function() {
+  var placeholder = document.querySelector('.rss-placeholder');
+  var widget = document.querySelector('rssapp-wall');
+
+  var checkWidgetLoaded = function() {
+    if (widget && widget.offsetHeight > 0) { // Check if widget has non-zero height, indicating it's loaded
+      placeholder.classList.add('d-none'); // Hide the placeholder
+      widget.classList.remove('d-none'); // Show the widget
+    } else {
+      setTimeout(checkWidgetLoaded, 500); // Check again in 500 milliseconds if not loaded
+    }
+  };
+
+  // Start checking if the widget has loaded
+  setTimeout(checkWidgetLoaded, 100); // Small delay to ensure widget script might have run
 });
 
-
-// Simulate RSS widget loading process
-setTimeout(() => {
-  // Replace the skeleton loader with the actual RSS widget HTML
-  document.getElementById("rss-widget-placeholder").outerHTML = `
-    <div>
-      <rssapp-wall id="tbrgVDoPyljZlaWS"></rssapp-wall>
-      <script src="https://widget.rss.app/v1/wall.js" type="text/javascript" async></script>
-    </div>
-  `;
-}, 1000); // Replace 3000ms with your actual widget load time if needed
